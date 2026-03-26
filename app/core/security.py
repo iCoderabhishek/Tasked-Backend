59fd1914b10c1cd350f5beff471d95ceccb6964a
Abhishek Jha <iamabhishek1310@gmail.com>
2026-03-26 21:48:56 +0530

    feature: added auth with validation

---
+++ app/core/security.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from passlib.context import CryptContext
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str):
+    if len(password.encode()) > 72:
+        raise ValueError("Password too long (max 72 bytes)")
+    return pwd_context.hash(password)
+
+
+def verify_password(plain, hashed):
+    return pwd_context.verify(plain, hashed)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
---
+++ app/schemas/user.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, EmailStr, Field, field_validator
+import re
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=8, max_length=20)
+
+    @field_validator("password")
+    def validate_password(cls, v):
+        if not re.search(r"[A-Za-z]", v):
+            raise ValueError("Must include a letter")
+        if not re.search(r"\d", v):
+            raise ValueError("Must include a number")
+        return v
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=8, max_length=20)
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
