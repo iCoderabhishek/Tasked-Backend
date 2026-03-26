910d4b6d8fc504d2d073cc6ce839817691a2ddda
Abhishek Jha <iamabhishek1310@gmail.com>
2026-03-27 00:57:37 +0530

    Fix: added cors and procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: prisma py fetch && prisma generate && uvicorn app.main:app --host 0.0.0.0 --port 8000
--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.router import api_router
 from app.core.db import db
 
 app = FastAPI(title="Backend", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
